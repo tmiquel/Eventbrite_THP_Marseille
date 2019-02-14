b82aa5ae08ba935617bde87c9d6f48da508e3fa6
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-02-14 09:35:47 +0100

    Attendance form woith Stripe added'

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -14,39 +14,78 @@ class AttendancesController < ApplicationController
   def show; end
 
   # GET /attendances/new
+
   def new
-    event = Event.first
-
-    @attendance = Attendance.new(attendee_id: current_user.id, event_id: session[:last_event].id, stripe_customer_id: rand(10))
-    if @attendance.save
-      redirect_to event_path(@attendance.event.id), notice: 'Attendance was successfully created.'
-    else
-      redirect_to event_path(@attendance.event.id), alert: 'Error. Attendance has not been created!'
-    end
+    @event = Event.find(params[:event_id])
+    @striprice = @event.price*100
+    @attendance
   end
 
-  # GET /attendances/1/edit
-  def edit; end
-
-  # POST /attendances
-  # POST /attendances.json
   def create
-    byebug
+    @event = Event.find(params[:event_id])
+    @striprice = @event.price*100
 
-    event = Event.first
+    customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+  )
 
-    @attendance = Attendance.new(attendee_id: current_user.id, event_id: event, stripe_customer_id: rand(10))
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @striprice,
+    :description => 'Rails Stripe customer',
+    :currency    => 'usd'
+  )
 
-    respond_to do |format|
-      if @attendance.save
-        format.html { redirect_to @attendance, notice: 'Attendance was successfully created.' }
-        format.json { render :show, status: :created, location: @attendance }
-      else
-        format.html { render :new }
-        format.json { render json: @attendance.errors, status: :unprocessable_entity }
-      end
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_event_attendance_path(@event)
+    
+    if customer&&charge
+      att = Attendance.create(attendee: current_user, event: @event, stripe_customer_id: charge.customer)
+      redirect_to root_path
+    else
+      redirect_to new_event_attendance_path(@event)
     end
-  end
+
+end
+
+
+
+
+  # def new
+  #   # event = Event.first
+
+  #   @attendance = Attendance.new(attendee_id: current_user.id, event_id: params[event.id], stripe_customer_id: rand(10))
+  #   if @attendance.save
+  #     redirect_to event_path(@attendance.event.id), notice: 'Attendance was successfully created.'
+  #   else
+  #     redirect_to event_path(@attendance.event.id), alert: 'Error. Attendance has not been created!'
+  #   end
+  # end
+
+  # # GET /attendances/1/edit
+  # def edit; end
+
+  # # POST /attendances
+  # # POST /attendances.json
+  # def create
+  #   byebug
+
+  #   event = Event.first
+
+  #   @attendance = Attendance.new(attendee_id: current_user.id, event_id: event, stripe_customer_id: rand(10))
+
+  #   respond_to do |format|
+  #     if @attendance.save
+  #       format.html { redirect_to @attendance, notice: 'Attendance was successfully created.' }
+  #       format.json { render :show, status: :created, location: @attendance }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @attendance.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /attendances/1
   # PATCH/PUT /attendances/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   devise_for :users, path_prefix: 'my'
   resources :users
-  resources :attendances
-  resources :events
+  resources :events do
+		resources :attendances
+	end  
   root to: 'events#index'
   resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
